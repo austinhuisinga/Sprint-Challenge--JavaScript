44f66227f96f4709ae239aedc12cc4cab3c77136
austinhuisinga <57517058+austinhuisinga@users.noreply.github.com>
2019-12-20 09:43:38 -0700

    adv.arrays.r2 complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -145,8 +145,11 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map((cv) => {
+  return cv.animal_name.toLowerCase();
+})
 console.log(lowCaseAnimalNames);
+
 
 /* Request 3: .filter() 
 
