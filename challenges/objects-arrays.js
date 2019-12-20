fa110aac4ad3e9d0e49dd916f08e8ab006db0c9b
austinhuisinga <57517058+austinhuisinga@users.noreply.github.com>
2019-12-20 09:19:56 -0700

    arrays.r1 complete.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -81,6 +81,10 @@ const graduates = [
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+graduates.forEach((obj) => {
+  return universities.push(obj.university);
+});
+universities.sort();
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
